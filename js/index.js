30d021f5609abf04b00e847e46d7b52701a8f10b
whitestorm2346 <andrew770426@gmail.com>
2023-03-15 09:56:17 +0800

    Finished recreate some inner pages using RWD

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 
 
 var optionCount = 0;
-$(function(){
+$(function () {
     var url = window.location;
     var urlAux = url.toString().split("=");
     var option = urlAux[1];
@@ -12,10 +12,10 @@ $(function(){
     changeFrameURL(optionCount);
 });
 
-function changeFrameURL(option){
-    if(option==null)document.getElementById("frame_content").src= 'index_main.html';
+function changeFrameURL(option) {
+    if (option == null) document.getElementById("frame_content").src = 'index_main.html';
 
-    switch (parseInt(option)){
+    switch (parseInt(option)) {
         case 0:
             document.getElementById("frame_content").src = 'search.html';
             break;
@@ -27,7 +27,7 @@ function changeFrameURL(option){
             break;
 
         case 3:
-            document.getElementById("frame_content").src='question.html ';
+            document.getElementById("frame_content").src = 'question.html ';
             break;
         case 4:
             document.getElementById("frame_content").src = 'law.html';
@@ -43,7 +43,7 @@ function changeFrameURL(option){
             document.getElementById("frame_content").src = 'index_eng.html';
             break;
         case 8:
-            document.getElementById("frame_content").src = 'index.html';     
+            document.getElementById("frame_content").src = 'index.html';
         case 201:
             document.getElementById("frame_content").src = 'notice_201.html';
             break;
@@ -51,65 +51,67 @@ function changeFrameURL(option){
             document.getElementById("frame_content").src = 'notice_202.html';
             break;
         case 203:
-            document.getElementById("frame_content").src='notice_203.html';
+            document.getElementById("frame_content").src = 'notice_203.html';
             break;
         case 204:
-            document.getElementById("frame_content").src='notice_204.html';
+            document.getElementById("frame_content").src = 'notice_204.html';
             break;
         case 401:
-            document.getElementById("frame_content").src='law_401.html';
+            document.getElementById("frame_content").src = 'law_401.html';
             break;
         case 402:
-            document.getElementById("frame_content").src='law_402.html';
+            document.getElementById("frame_content").src = 'law_402.html';
             break;
         case 403:
-            document.getElementById("frame_content").src='law_403.html';
+            document.getElementById("frame_content").src = 'law_403.html';
             break;
         case 404:
-            document.getElementById("frame_content").src='law_404.html';
+            document.getElementById("frame_content").src = 'law_404.html';
             break;
         case 405:
-            document.getElementById("frame_content").src='law_405.html';
+            document.getElementById("frame_content").src = 'law_405.html';
             break;
         case 406:
-            document.getElementById("frame_content").src='law_406.html';
+            document.getElementById("frame_content").src = 'law_406.html';
             break;
         case 407:
-            document.getElementById("frame_content").src='law_407.html';
+            document.getElementById("frame_content").src = 'law_407.html';
             break;
         case 408:
-            document.getElementById("frame_content").src='law_408.html';
+            document.getElementById("frame_content").src = 'law_408.html';
             break;
         case 409:
-            document.getElementById("frame_content").src='law_409.html';
+            document.getElementById("frame_content").src = 'law_409.html';
             break;
         case 501:
-            document.getElementById("frame_content").src='video_501.html';
+            document.getElementById("frame_content").src = 'video_501.html';
             break;
         case 502:
-            document.getElementById("frame_content").src='video_502.html';
+            document.getElementById("frame_content").src = 'video_502.html';
             break;
         case 503:
-            document.getElementById("frame_content").src='video_503.html';
+            document.getElementById("frame_content").src = 'video_503.html';
             break;
         case 504:
-            document.getElementById("frame_content").src='video_504.html';
+            document.getElementById("frame_content").src = 'video_504.html';
             break;
         case 7:
-            document.getElementById("frame_content").src='form_student.html';
+            document.getElementById("frame_content").src = 'form_student.html';
             break;
         case 8:
-            document.getElementById("frame_content").src='form_landlord.html';
+            document.getElementById("frame_content").src = 'form_landlord.html';
             break;
         case 9:
-            document.getElementById("frame_content").src='contract.html';
-        break;
+            document.getElementById("frame_content").src = 'contract.html';
+            break;
 
     }
 }
 function goPay(option) {
-    document.location.href="question_.main.html";
+    document.location.href = "question_.main.html";
 }
 function changeOption(option) {
-    document.location.href="index.html?option="+option;
+    document.location.href = "index.html?option=" + option;
 }
+
+
