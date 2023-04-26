bebf6fab6a29b250313442e901271bdaee7cbcf2
whitestorm2346 <andrew770426@gmail.com>
2023-04-26 09:57:53 +0800

    Translating law_402_eng.html and law_403_eng.html

--- js/index.js
+++ js/index.js
@@ -57,6 +57,7 @@ function changeFrameURL(option) {
             break;
         case 8:
             document.getElementById("frame_content").src = 'index.html';
+            break;
         case 201:
             document.getElementById("frame_content").src = 'notice_201.html';
             break;
@@ -91,49 +92,49 @@ function changeFrameURL(option) {
             document.getElementById("frame_content").src = 'law_402.html';
             break;
         case 4021:
-            document.getElementById("frame_content").src = 'law_402.html';
+            document.getElementById("frame_content").src = 'law_402_eng.html';
             break;
         case 403:
             document.getElementById("frame_content").src = 'law_403.html';
             break;
         case 4031:
-            document.getElementById("frame_content").src = 'law_403.html';
+            document.getElementById("frame_content").src = 'law_403_eng.html';
             break;
         case 404:
             document.getElementById("frame_content").src = 'law_404.html';
             break;
         case 4041:
-            document.getElementById("frame_content").src = 'law_404.html';
+            document.getElementById("frame_content").src = 'law_404_eng.html';
             break;
         case 405:
             document.getElementById("frame_content").src = 'law_405.html';
             break;
         case 4051:
-            document.getElementById("frame_content").src = 'law_405.html';
+            document.getElementById("frame_content").src = 'law_405_eng.html';
             break;
         case 406:
             document.getElementById("frame_content").src = 'law_406.html';
             break;
         case 4061:
-            document.getElementById("frame_content").src = 'law_406.html';
+            document.getElementById("frame_content").src = 'law_406_eng.html';
             break;
         case 407:
             document.getElementById("frame_content").src = 'law_407.html';
             break;
         case 4071:
-            document.getElementById("frame_content").src = 'law_407.html';
+            document.getElementById("frame_content").src = 'law_407_eng.html';
             break;
         case 408:
             document.getElementById("frame_content").src = 'law_408.html';
             break;
         case 4081:
-            document.getElementById("frame_content").src = 'law_408.html';
+            document.getElementById("frame_content").src = 'law_408_eng.html';
             break;
         case 409:
             document.getElementById("frame_content").src = 'law_409.html';
             break;
         case 4091:
-            document.getElementById("frame_content").src = 'law_409.html';
+            document.getElementById("frame_content").src = 'law_409_eng.html';
             break;
         case 501:
             document.getElementById("frame_content").src = 'video_501.html';
