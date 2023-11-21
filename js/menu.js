f99680939952d4304a89d5809dd02a4eab394a88
whitestorm2346 <andrew770426@gmail.com>
2023-11-21 15:56:12 +0800

    trying to implement the dropdown menu, but still have some bugs

--- js/menu.js
+++ js/menu.js
@@ -8,24 +8,28 @@ const menu1 = document.querySelector("#menu-1");
 const menu2 = document.querySelector("#menu-2");
 
 navBtn1.addEventListener("click", function () {
+    menu1.style.maxHeight = '500px';
     menu1.classList.add("show-menu");
     closeBtn1.classList.remove("hide-btn");
     navBtn1.classList.add("hide-btn");
 });
 
 closeBtn1.addEventListener("click", function () {
+    menu1.style.maxHeight = '0';
     menu1.classList.remove("show-menu");
     navBtn1.classList.remove("hide-btn");
     closeBtn1.classList.add("hide-btn");
 });
 
 navBtn2.addEventListener("click", function () {
-    menu2.classList.add("show-menu");
+    menu2.style.maxHeight = '500px';
+    menu2.classList.add('show-menu');
     closeBtn2.classList.remove("hide-btn");
     navBtn2.classList.add("hide-btn");
 });
 
 closeBtn2.addEventListener("click", function () {
+    menu2.style.maxHeight = '0';
     menu2.classList.remove("show-menu");
     navBtn2.classList.remove("hide-btn");
     closeBtn2.classList.add("hide-btn");
