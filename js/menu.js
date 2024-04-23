1a32608e9b92b1c546326e4a3cffec83329b8b73
whitestorm2346 <andrew770426@gmail.com>
2024-04-23 11:55:36 +0800

    start to implement the display of inner menu

--- js/menu.js
+++ js/menu.js
@@ -1,44 +1,61 @@
-// Using RWD created by Tse-Hsun Yu on 2023/12/25
-
-// const navBtn1 = document.querySelector("#nav-btn-1");
-// const navBtn2 = document.querySelector("#nav-btn-2");
-// const closeBtn1 = document.querySelector("#close-btn-1");
-// const closeBtn2 = document.querySelector("#close-btn-2");
 const menubtn1 = document.querySelector("#toggler1");
-const menubtn2 = document.querySelector("#toggler2");
 const menu1 = document.querySelector("#menu-1");
-const menu2 = document.querySelector("#menu-2");
 
-// navBtn1.addEventListener("click", function () {
-//     menu1.classList.add("show-menu");
-//     closeBtn1.classList.remove("hide-btn");
-//     navBtn1.classList.add("hide-btn");
-// });
-
-// closeBtn1.addEventListener("click", function () {
-//     menu1.classList.remove("show-menu");
-//     navBtn1.classList.remove("hide-btn");
-//     closeBtn1.classList.add("hide-btn");
-// });
-
-// navBtn2.addEventListener("click", function () {
-//     menu2.classList.add("show-menu");
-//     closeBtn2.classList.remove("hide-btn");
-//     navBtn2.classList.add("hide-btn");
-// });
-
-// closeBtn2.addEventListener("click", function () {
-//     menu2.classList.remove("show-menu");
-//     navBtn2.classList.remove("hide-btn");
-//     closeBtn2.classList.add("hide-btn");
-// });
 
 menubtn1.addEventListener("click", function () {
-    if(menubtn1.checked) menu1.classList.add("show-menu");
-    else menu1.classList.remove("show-menu");
+    if(menubtn1.checked) {
+        menu1.classList.add("show-menu");
+        menu1.classList.remove("hide-menu");
+    }
+    else {
+        menu1.classList.add("hide-menu");
+        menu1.classList.remove("show-menu");
+    }
 });
 
-menubtn2.addEventListener("click", function () {
-    if(menubtn2.checked) menu2.classList.add("show-menu");
-    else menu2.classList.remove("show-menu");
-});
+
+function showInnerMenu(index) {
+    for(var i = 1; i <= 3; i++){
+        var hide_btn = document.querySelector("#hide-inner-menu-btn-" + index)
+        var show_btn = document.querySelector("#show-inner-menu-btn-" + index)
+        
+        if(i == index) {
+            show_btn.classList.add("hide-btn")
+            hide_btn.classList.remove("hide-btn")
+        }
+        else {
+            show_btn.classList.remove("hide-btn")
+            hide_btn.classList.add("hide-btn")
+        }
+    }
+
+    var inner_menu = document.querySelector("#inner-menu2-" + index)
+    inner_menu.classList.add("show-inner-menu")
+}
+
+function hideInnerMenu(index) {
+    for(var i = 1; i <= 3; i++){
+        var hide_btn = document.querySelector("#hide-inner-menu-btn-" + index)
+        var show_btn = document.querySelector("#show-inner-menu-btn-" + index)
+        
+        if(i == index) {
+            show_btn.classList.remove("hide-btn")
+            hide_btn.classList.add("hide-btn")
+        }
+        else {
+            show_btn.classList.add("hide-btn")
+            hide_btn.classList.remove("hide-btn")
+        }
+    }
+
+    var inner_menu = document.querySelector("#inner-menu2-" + index)
+    inner_menu.classList.remove("show-inner-menu")
+}
+
+for(var i = 1; i <= 3; i++){
+    var hide_btn = document.querySelector("#hide-inner-menu-btn-" + index)
+    var show_btn = document.querySelector("#show-inner-menu-btn-" + index)
+
+    show_btn.addEventListener("click", showInnerMenu(i))
+    hide_btn.addEventListener("click", hideInnerMenu(i))
+}
