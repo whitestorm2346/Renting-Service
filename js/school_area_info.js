6c62b9d7bec13c2db865b9c3d3d0ab71395950b8
whitestorm2346 <andrew770426@gmail.com>
2024-09-12 10:56:11 +0800

    Start to redesigning the school_area_info.html

--- js/school_area_info.js
+++ js/school_area_info.js
@@ -12,19 +12,28 @@ $(function () {
 
 function changeFrameURL(option) {
     if (option == null) {
-        document.getElementById("frame_school_area_info").src = 'communities.html';
+        document.getElementById("frame_school_area_info").src = 'medical.html';
     }
     switch (parseInt(option)) {
         case -1:
-        case 300:
-            document.getElementById("frame_school_area_info").src = 'communities.html';
+        case 302:
+            document.getElementById("frame_school_area_info").src = 'medical.html';
             break;
+        case 303:
+            document.getElementById("frame_school_area_info").src = 'firefighting.html';
+            break;
+        case 304:
+            document.getElementById("frame_school_area_info").src = 'police.html';
+            break;
+        // case 300:
+        //     document.getElementById("frame_school_area_info").src = 'communities.html';
+        //     break;
         // case 301:
         //     document.getElementById("frame_school_area_info").src = 'utility_area.html';
         //     break;
-        case 301:
-            document.getElementById("frame_school_area_info").src = 'common_pages/fixing.html';
-            break;
+        // case 301:
+        //     document.getElementById("frame_school_area_info").src = 'common_pages/fixing.html';
+        //     break;
     }
 }
 function changeOption2(option) {
