58985ab3cc1aca63f6e53789cfcbe9fe3439e216
whitestorm2346 <andrew770426@gmail.com>
2025-03-25 16:50:24 +0800

    Finished the first version of the pop up section

--- js/search.js
+++ js/search.js
@@ -12,9 +12,7 @@ const pages = [
     'index_main.html',
     'safe.html',
     'notice_index.html',
-    'question.html ',
     'law.html',
-    'video.html',
     'download.html',
     'notice_201.html',
     'notice_202.html',
@@ -29,17 +27,36 @@ const pages = [
     'law_407.html',
     'law_408.html',
     'law_409.html',
-    'video_501.html',
-    'video_502.html',
-    'video_503.html',
-    'video_504.html',
-    'form_student.html',
-    'form_landlord.html',
     'contract.html',
     'school_area_info.html',
     'rental_safety.html',
     'rental_info.html',
 ];
+
+const page_tag = {
+    'index_main.html': "主頁公告",
+    'safe.html': "租屋安全總覽",
+    'notice_index.html': "租屋注意事項",
+    'law.html': "相關法律問題",
+    'download.html': "表單下載",
+    'notice_201.html': "租屋建議事項",
+    'notice_202.html': "退租還屋注意事項",
+    'notice_203.html': "租屋教戰守則",
+    'notice_204.html': "常見租屋糾紛",
+    'law_401.html': "存證信函",
+    'law_402.html': "辦理房屋租賃契約公證",
+    'law_403.html': "選擇簽訂對象",
+    'law_404.html': "租賃法律關係概說",
+    'law_405.html': "租屋糾紛解決",
+    'law_406.html': "租屋相關法律條文",
+    'law_407.html': "租約內容注意事項",
+    'law_408.html': "租約之履行",
+    'law_409.html': "租約相關法律",
+    'contract.html': "租屋懶人包",
+    'school_area_info.html': "賃居生活資訊",
+    'rental_safety.html': "租屋安全",
+    'rental_info.html': "租屋資訊",
+}
 
 const search_input = document.getElementById("searchInput");
 const search_btn = document.getElementById("searchBtn");
@@ -80,18 +97,59 @@ async function searchContent(keyword) {
 
             if (text.includes(keyword)) {
                 results.push(page);
-                console.log(page);
+                // console.log(page);
             }
         } catch (error) {
             console.error(`無法讀取 ${page}`, error);
         }
     }
 
-    const search_popup = document.getElementById("search-results-popup");
+    const search_popup = document.getElementById("searchPopup");
 
     if(results.length === 0){ 
         console.log("查無相關訊息");
 
         search_popup.classList.add("search-results-no-result");
+        search_popup.classList.remove("search-results-popup")
+        search_popup.innerHTML = `
+            <div class="title">搜尋<span>失敗</span></div>
+            <div class="content">
+                <p>查無與搜尋關鍵字相符之頁面內容</p>
+            </div>
+            <button id="popup-close-btn" class="close">關閉</button>
+        `
+
+        document.getElementById("popup-close-btn").addEventListener("click", function () {
+            search_popup.classList.add("search-results-popup")
+            search_popup.classList.remove("search-results-no-result")
+            search_popup.innerHTML = ""
+        });
+    }
+    else {
+        console.log(results);
+
+        innerUl = ""
+
+        for(const page of results) {
+            innerUl += `<li class="link"><a href="">${page_tag[page]}</a></li>`
+        }
+
+        search_popup.classList.add("search-results-show-results");
+        search_popup.classList.remove("search-results-popup")
+        search_popup.innerHTML = `
+            <div class="title">相關頁面<span>鏈結</span></div>
+            <div class="content">
+                <ul class="links">
+                    ${innerUl}
+                </ul>
+            </div>
+            <button id="popup-close-btn" class="close">關閉</button>
+        `
+
+        document.getElementById("popup-close-btn").addEventListener("click", function () {
+            search_popup.classList.add("search-results-popup")
+            search_popup.classList.remove("search-results-show-results")
+            search_popup.innerHTML = ""
+        });
     }
 }
