f08cc185390d7190dfa51c79e5a6a56a9fe8dc40
whitestorm2346 <andrew770426@gmail.com>
2025-05-08 17:01:45 +0800

    增加至頂圖示

--- js/index.js
+++ js/index.js
@@ -85,7 +85,7 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 function goPay(option) {
-    document.location.href = "question_.main.html";
+    document.location.href = "question_main.html";
 }
 
 function changeOption(option) {
@@ -96,4 +96,25 @@ function changeOptionEng(option) {
     document.location.href = "index_eng.html?option=" + option;
 }
 
-
+$(document).ready(function () {
+    // 捲軸偵測距離頂部超過 50 才顯示按鈕
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 50) {
+        if ($(".back-top").hasClass("hide")) {
+          $(".back-top").toggleClass("hide");
+        }
+      } else {
+        $(".back-top").addClass("hide");
+      }
+    });
+  
+    // 點擊按鈕回頂部
+    $(".back-top").on("click", function (event) {
+      $("html, body").animate(
+        {
+          scrollTop: 0
+        },
+        500 // 回頂部時間為 500 毫秒
+      );
+    });
+  });
